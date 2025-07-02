e1b936e47b1eeb05d7f5ef2a59b0cc7403e622c8
dimanu-py <dimanu.py@gmail.com>
2025-07-02 09:12:17 +0200

    test(for-registering-usage): implement helper method to make post request to test endpoint

--- test/driving/for_registering_usage/v1/test_register_usage_router.py
+++ test/driving/for_registering_usage/v1/test_register_usage_router.py
@@ -2,13 +2,14 @@ import pytest
 from fastapi.testclient import TestClient
 
 from src.driving.application import app
+from src.monitoring.driving.http.http_response import HttpResponse
 
 
 @pytest.mark.acceptance
 class TestRegisterUsageRouter:
     def setup_method(self) -> None:
         self._client = TestClient(app)
-        self._response = None
+        self._response = HttpResponse(message="Default response", status_code=200)
 
     def test_should_register_usage_of_instant_python_command(self) -> None:
         request_body = {
@@ -20,9 +21,15 @@ class TestRegisterUsageRouter:
                 "built_in_features": ["value_objects", "logger"],
             },
         }
-        
+
         self._response = self._when_a_post_request_is_made_to(
             "/app/v1/monitoring/usage", body=request_body
         )
 
         self._then_response_should_be_successful()
+
+    def _when_a_post_request_is_made_to(
+        self, endpoint: str, body: dict
+    ) -> HttpResponse:
+        with self._client as client:
+            return client.post(endpoint, json=body)  # type: ignore
