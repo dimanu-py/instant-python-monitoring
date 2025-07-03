eccda69cfa3de17e5b7723484c23d35cb92d6ff8
dimanu-py <dimanu.py@gmail.com>
2025-07-03 19:02:09 +0200

    test(monitoring): use usage information mother in unit test

--- test/monitoring/driving/for_registering_usage/test_usage_registrar.py
+++ test/monitoring/driving/for_registering_usage/test_usage_registrar.py
@@ -3,7 +3,9 @@ from doublex import Spy, assert_that, called
 
 from src.monitoring.driven.for_sending_usage.for_sending_usage import ForSendingUsage
 from src.monitoring.driving.for_registering_usage.usage_registrar import UsageRegistrar
+from src.monitoring.usage.usage_information import UsageInformationPrimitives, UsageInformation
 from test.monitoring.driving.for_registering_usage.register_usage_command_mother import RegisterUsageCommandMother
+from test.monitoring.usage.usage_information_primitives_mother import UsageInformationPrimitivesMother
 
 
 @pytest.mark.unit
@@ -14,13 +16,14 @@ class TestUsageRegistrar:
 
     def test_should_register_command_usage(self) -> None:
         command = RegisterUsageCommandMother.any()
+        usage_information_primitives = UsageInformationPrimitivesMother.from_command(command)
 
         self._usage_registrar.execute(command)
 
-        self._should_have_send_usage_information(command.to_primitives())
+        self._should_have_send_usage_information(usage_information_primitives)
 
-    def _should_have_send_usage_information(self, expected_information: dict) -> None:
+    def _should_have_send_usage_information(self, expected_information: UsageInformationPrimitives) -> None:
         assert_that(
             self._sender.send_information,
-            called().with_args(info=expected_information),
+            called().with_args(info=UsageInformation(**expected_information)),
         )
