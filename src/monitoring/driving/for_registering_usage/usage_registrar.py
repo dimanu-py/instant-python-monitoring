44475145fce69778958c92abc260861216853562
dimanu-py <dimanu.py@gmail.com>
2025-07-03 09:11:02 +0200

    feat(monitoring): define how use case communicates with sender to send command usage information

--- src/monitoring/driving/for_registering_usage/usage_registrar.py
+++ src/monitoring/driving/for_registering_usage/usage_registrar.py
@@ -5,4 +5,4 @@ from src.monitoring.driving.for_registering_usage.register_usage_command import 
 
 class UsageRegistrar:
     def execute(self, command: RegisterUsageCommand) -> None:
-        raise NotImplementedError
+        self._sender.send_information(command.to_primitives())
