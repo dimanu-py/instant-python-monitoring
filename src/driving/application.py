ebdfc41976466b00c121f430d3b48dd92e6c2147
dimanu-py <dimanu.py@gmail.com>
2025-07-03 07:41:56 +0200

    feat(driving): modify error handlers to generate error data and return ApiError converted to json

--- src/driving/application.py
+++ src/driving/application.py
@@ -1,22 +1,38 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 
+from src.driving.http.api_response import Error, ApiError
 from src.driving.v1 import routes as v1
 from src.monitoring.domain_error import DomainError
-from src.monitoring.driving.http.responses import (
-    InternalServerError,
-    HttpResponse,
-    UnprocessableEntity,
-)
+
 
 app = FastAPI()
 app.include_router(v1.router)
 
 
 @app.exception_handler(Exception)
-async def unexpected_exception_handler(_: Request, exc: Exception) -> HttpResponse:
-    return InternalServerError()
+async def unexpected_exception_handler(
+    request: Request, exc: Exception
+) -> JSONResponse:
+    error = Error(
+        type="internal_server_error",
+        detail="An unexpected error occurred",
+        source=request.url.path,
+        base_error=str(exc),
+    )
+    return ApiError(errors=[error]).to_json(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+    )
 
 
 @app.exception_handler(DomainError)
-async def domain_error_handler(_: Request, exc: DomainError) -> HttpResponse:
-    return UnprocessableEntity(message=exc.message)
+async def domain_error_handler(request: Request, exc: DomainError) -> JSONResponse:
+    error = Error(
+        type=exc.type,
+        detail=exc.message,
+        source=request.url.path,
+        base_error=str(exc),
+    )
+    return ApiError(errors=[error]).to_json(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
