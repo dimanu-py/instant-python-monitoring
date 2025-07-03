6a20aed6340338e1c902f32cd679bbab24d109bf
dimanu-py <dimanu.py@gmail.com>
2025-07-03 07:54:10 +0200

    feat(driving): implement request model for registering usage

--- src/driving/v1/for_registering_usage/register_usage_router.py
+++ src/driving/v1/for_registering_usage/register_usage_router.py
@@ -4,6 +4,13 @@ from pydantic import BaseModel
 router = APIRouter(prefix="/monitoring", tags=["monitoring"])
 
 
+class RegisterUsageRequest(BaseModel):
+    command: str
+    version: str
+    platform: str
+    template_data: dict
+
+
 @router.post("/usage", status_code=status.HTTP_204_NO_CONTENT)
 def register_usage(request: RegisterUsageRequest) -> None:
     raise NotImplementedError
