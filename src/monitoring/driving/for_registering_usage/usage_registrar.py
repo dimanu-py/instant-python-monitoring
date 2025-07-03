fb63067c525f2ba7115d2b48281eea11ebea1e10
dimanu-py <dimanu.py@gmail.com>
2025-07-03 18:55:50 +0200

    feat(monitoring): create usage information to validate data and pass that object to sender

--- src/monitoring/driving/for_registering_usage/usage_registrar.py
+++ src/monitoring/driving/for_registering_usage/usage_registrar.py
@@ -2,6 +2,7 @@ from src.monitoring.driven.for_sending_usage.for_sending_usage import ForSending
 from src.monitoring.driving.for_registering_usage.register_usage_command import (
     RegisterUsageCommand,
 )
+from src.monitoring.usage.usage_information import UsageInformation
 
 
 class UsageRegistrar:
@@ -9,4 +10,10 @@ class UsageRegistrar:
         self._sender = for_sending_usage
 
     def execute(self, command: RegisterUsageCommand) -> None:
-        self._sender.send_information(info=command.to_primitives())
+        usage_information = UsageInformation(
+            command=command.command,
+            version=command.version,
+            platform=command.platform,
+            template_data=command.template_data,
+        )
+        self._sender.send_information(info=usage_information)
