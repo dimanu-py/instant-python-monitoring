c6d933535f1555b7d49a00dcd5895c6e429f4326
dimanu-py <dimanu.py@gmail.com>
2025-07-03 18:54:07 +0200

    refactor(monitoring): model UsageInformation using value objects instead of primitives

--- src/monitoring/usage/usage_information.py
+++ src/monitoring/usage/usage_information.py
@@ -1,6 +1,14 @@
+from src.monitoring.usage.command import Command
+from src.monitoring.usage.platform import Platform
+from src.monitoring.usage.template_data import TemplateData
+from src.monitoring.usage.version import Version
+
+
 class UsageInformation:
-    def __init__(self, command: str, version: str, platform: str, template_data: dict) -> None:
-        self._command = command
-        self._version = version
-        self._platform = platform
-        self._template_data = template_data
+    def __init__(
+        self, command: str, version: str, platform: str, template_data: dict
+    ) -> None:
+        self._command = Command(command)
+        self._version = Version(version)
+        self._platform = Platform(platform)
+        self._template_data = TemplateData(template_data)
