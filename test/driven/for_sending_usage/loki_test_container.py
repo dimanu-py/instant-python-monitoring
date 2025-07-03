a257316c7861521b86ee3ae70811b91fb75972b0
dimanu-py <dimanu.py@gmail.com>
2025-07-03 16:29:35 +0200

    test(driven): create test container class wrapper for loki docker container

---
+++ test/driven/for_sending_usage/loki_test_container.py
@@ -0,0 +1,16 @@
+from testcontainers.core.container import DockerContainer
+
+
+class LokiTestContainer(DockerContainer):
+    _LOKI_IMAGE = "grafana/loki:main-5c02493"
+    _LOKI_PORT = 3100
+
+    def __init__(self) -> None:
+        super().__init__(self._LOKI_IMAGE)
+        self.with_exposed_ports(self._LOKI_PORT)
+        self.with_command(["-config.file=/etc/loki/local-config.yaml"])
+
+    def get_base_url(self) -> str:
+        host = self.get_container_host_ip()
+        port = self.get_exposed_port(self._LOKI_PORT)
+        return f"http://{host}:{port}"
