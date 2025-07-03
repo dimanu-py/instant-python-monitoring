b91c63be42fcf86f59971ea3c49856fd76527966
dimanu-py <dimanu.py@gmail.com>
2025-07-03 19:07:25 +0200

    feat(monitoring): let send_information method receive UsageInformation object instead of primitives

--- src/monitoring/driven/for_sending_usage/for_sending_usage.py
+++ src/monitoring/driven/for_sending_usage/for_sending_usage.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 
+from src.monitoring.usage.usage_information import UsageInformation
+
 
 class ForSendingUsage(ABC):
     @abstractmethod
-    def send_information(self, info: dict) -> None:
+    def send_information(self, info: UsageInformation) -> None:
         raise NotImplementedError
