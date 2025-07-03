f86990a283c75eb439016db2761e364e7d76c254
dimanu-py <dimanu.py@gmail.com>
2025-07-03 19:23:14 +0200

    refactor(monitoring): modify argument type and how its data is handled when sending usage to loki

--- src/driven/for_sending_usage/loki_client.py
+++ src/driven/for_sending_usage/loki_client.py
@@ -4,13 +4,14 @@ import time
 import requests
 
 from src.monitoring.driven.for_sending_usage.for_sending_usage import ForSendingUsage
+from src.monitoring.usage.usage_information import UsageInformation
 
 
 class LokiClient(ForSendingUsage):
     def __init__(self, url: str) -> None:
         self._push_endpoint = f"{url}/loki/api/v1/push"
 
-    def send_information(self, info: dict) -> None:
+    def send_information(self, info: UsageInformation) -> None:
         current_time_ns = int(time.time_ns())
         
         payload = {
@@ -18,11 +19,11 @@ class LokiClient(ForSendingUsage):
                 {
                     "stream": {
                         "job": "command-usage",
-                        "command": info["command"],
-                        "version": info["version"],
+                        "command": info.command.value,
+                        "version": info.version.value,
                     },
                     "values": [
-                        [str(current_time_ns), json.dumps(info)]
+                        [str(current_time_ns), json.dumps(info.value)]
                     ]
                 }
             ]
