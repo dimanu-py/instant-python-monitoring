d64fc8cffb69e380a5fc7183afadb543cea6c8a9
dimanu-py <dimanu.py@gmail.com>
2025-07-03 19:00:12 +0200

    feat(monitoring): create typed dict to be able to type user information primitives

--- src/monitoring/usage/usage_information.py
+++ src/monitoring/usage/usage_information.py
@@ -1,3 +1,5 @@
+from typing import TypedDict
+
 from src.monitoring.usage.command import Command
 from src.monitoring.usage.platform import Platform
 from src.monitoring.usage.template_data import TemplateData
@@ -12,3 +14,10 @@ class UsageInformation:
         self._version = Version(version)
         self._platform = Platform(platform)
         self._template_data = TemplateData(template_data)
+
+
+class UsageInformationPrimitives(TypedDict):
+    command: str
+    version: str
+    platform: str
+    template_data: dict
