88452d4360f77573803bc158a12eff0d56269bde
dimanu-py <dimanu.py@gmail.com>
2025-07-03 19:22:49 +0200

    test(monitoring): implement 'any' method to generate random data for usage information

--- test/monitoring/usage/usage_information_primitives_mother.py
+++ test/monitoring/usage/usage_information_primitives_mother.py
@@ -2,9 +2,23 @@ from src.monitoring.driving.for_registering_usage.register_usage_command import 
     RegisterUsageCommand,
 )
 from src.monitoring.usage.usage_information import UsageInformationPrimitives
+from test.monitoring.usage.command_primitives_mother import CommandPrimitivesMother
+from test.random_generator import RandomGenerator
 
 
 class UsageInformationPrimitivesMother:
+    @staticmethod
+    def any() -> UsageInformationPrimitives:
+        return UsageInformationPrimitives(
+            command=CommandPrimitivesMother.any(),
+            version=RandomGenerator.version(),
+            platform=RandomGenerator.operating_system(),
+            template_data={
+                "name": "standard_project",
+                "built_in_features": ["logger", "makefile"],
+            },
+        )
+
     @staticmethod
     def from_command(command: RegisterUsageCommand) -> UsageInformationPrimitives:
         return UsageInformationPrimitives(
