bbb929793228c27d8e987c07d61eca102ba375e4
dimanu-py <dimanu.py@gmail.com>
2025-07-03 09:32:07 +0200

    test(monitoring): implement helper method to verify ForSendingUsage has been called with expected information

--- test/monitoring/driving/for_registering_usage/test_usage_registrar.py
+++ test/monitoring/driving/for_registering_usage/test_usage_registrar.py
@@ -1,14 +1,15 @@
 import pytest
-from doublex import Spy
+from doublex import Spy, assert_that, called
 
 from src.monitoring.driven.for_sending_usage.for_sending_usage import ForSendingUsage
-from src.monitoring.driving.for_registering_usage.register_usage_command import RegisterUsageCommand
+from src.monitoring.driving.for_registering_usage.register_usage_command import (
+    RegisterUsageCommand,
+)
 from src.monitoring.driving.for_registering_usage.usage_registrar import UsageRegistrar
 
 
 @pytest.mark.unit
 class TestUsageRegistrar:
-    @pytest.mark.xfail
     def test_should_register_command_usage(self) -> None:
         command = RegisterUsageCommand(
             command="init",
@@ -20,8 +21,14 @@ class TestUsageRegistrar:
             },
         )
         sender = Spy(ForSendingUsage)
-        usage_registrar = UsageRegistrar(for_sending_usage=sender)
+        usage_registrar = UsageRegistrar(for_sending_usage=sender)  # type: ignore
 
         usage_registrar.execute(command)
 
-        self._should_have_send_usage_information()
+        self._should_have_send_usage_information(command.to_primitives(), sender)
+
+    def _should_have_send_usage_information(self, expected_information: dict, sender) -> None:
+        assert_that(
+            sender.send_information,
+            called().with_args(info=expected_information),
+        )
